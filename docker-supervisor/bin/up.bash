c5f2089dfa70e1d887cec0bac1f31b923febf006
Sho <shovity@gmail.com>
2025-03-18 17:49:46 +0000

    use current user

--- docker-supervisor/bin/up.bash
+++ docker-supervisor/bin/up.bash
@@ -1,6 +1,11 @@
 yarn
 sudo cp ./docker-supervisor.service /etc/systemd/system/
-sudo sed -i "s/{app_dir}/$(pwd | sed 's/[\/&]/\\&/g')/" /etc/systemd/system/docker-supervisor.service
+sudo sed -i "s/{app_dir}/$(pwd | sed 's/[\/&]/\\&/g')/" -i "s/{user}/$(whoami)/" /etc/systemd/system/docker-supervisor.service
+sudo sed -i "s/{user}/$(whoami)/" /etc/systemd/system/docker-supervisor.service
 sudo systemctl enable docker-supervisor
 sudo systemctl start docker-supervisor.service
 sudo systemctl status docker-supervisor.service
+
+cp ./docker-supervisor.service zz
+sed -i "s/{app_dir}/$(pwd | sed 's/[\/&]/\\&/g')/" zz
+sed -i "s/{user}/$(whoami)/" zz
