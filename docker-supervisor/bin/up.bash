2da8293e396842eec37819ec5f4112569b6bc392
Sho <shovity@gmail.com>
2025-07-10 13:17:55 +0000

    rm unsed sed

--- docker-supervisor/bin/up.bash
+++ docker-supervisor/bin/up.bash
@@ -1,7 +1,6 @@
 yarn
 sudo cp ./docker-supervisor.service /etc/systemd/system/
 sudo sed -i "s/{app_dir}/$(pwd | sed 's/[\/&]/\\&/g')/" -i "s/{user}/$(whoami)/" /etc/systemd/system/docker-supervisor.service
-sudo sed -i "s/{user}/$(whoami)/" /etc/systemd/system/docker-supervisor.service
 sudo systemctl enable docker-supervisor
 sudo systemctl start docker-supervisor.service
 sudo systemctl status docker-supervisor.service
