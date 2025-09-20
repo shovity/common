79f4c73cf0c9f3f9bc8d292860f677ea697ade58
Sho <shovity@gmail.com>
2025-09-20 03:32:37 +0000

    update script

--- openvnp-server/bin/init-server.sh
+++ openvnp-server/bin/init-server.sh
@@ -1,3 +1,10 @@
+if [ -z "$1" ]; then
+    echo "Error: Domain name argument is required"
+    echo "Usage: $0 vpn.example.com"
+    exit 1
+fi
+
+
 mkdir -p clients
 docker run -v $PWD/data:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://$1
 docker run -v $PWD/data:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
